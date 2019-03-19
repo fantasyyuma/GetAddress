0e5cd0cff34ba561b89a044ac0ce30e140e84234
fantasyyuma <31132962+fantasyyuma@users.noreply.github.com>
2019-03-19 22:00:54 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GetAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Dns.GetHostName();
+            Console.WriteLine("主機名稱 ：{0} ", s);
+            IPAddress[] IPS = Dns.GetHostEntry(s).AddressList;
+            IEnumerator iEnums = IPS.GetEnumerator();
+            while (iEnums.MoveNext())
+            {
+                Console.WriteLine("IP : {0}", iEnums.Current.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
